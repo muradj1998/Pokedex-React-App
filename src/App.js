86b474bb4b3d613fae0b177d1656f9be453c7cf0
Murad Jafarly <muradjafarlybusiness@gmail.com>
2021-08-30 21:55:27 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import './App.css';
+import { PokeList, DetailView, Pokemon } from './components'
+
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      pokemon: {
+        id: 1,
+        name: 'bulbasaur',
+        type: 'grass',
+        weight: 69,
+        height: 7,
+        sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+      }
+    }
+
+    this.handleOnClick = this.handleOnClick.bind(this)
+  }
+
+  handleOnClick(id) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`)
+    .then(response => response.json())
+    .then(data => {
+      const pokemon = new Pokemon(data)
+      this.setState({ pokemon })
+    })
+    .catch(err => console.log(err))
+  }
+  
+  render () {
+    return (
+      <div className="App">
+     <PokeList handleOnClick={this.handleOnClick} />
+     <DetailView pokemon={this.state.pokemon}/>
+      </div>
+    )
+  }
+}
+
+export default App;
+ 
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as PokeList } from './PokeList'
+export { default as DetailView } from './DetailView'
+export { default as Pokemon } from './Pokemon'
